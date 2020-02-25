7e7aaa8bee22981da99b5af27bbf7e898cc11a7e
peiyunluo <peiyunluo@icloud.com>
2020-02-25 12:41:37 +0800

    add Userinfo (å¢å userinfoå®ä½)

---
+++ src/main/java/Aamir/model/entity/BaseEntity.java
@@ -0,0 +1,63 @@
+package Aamir.model.entity;
+
+
+import Aamir.utils.DateUtils;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * @author peiyunluo@icloud.com
+ * @date 2020/2/24 15:27
+ */
+@Data
+@ToString
+@MappedSuperclass
+@EqualsAndHashCode
+public class BaseEntity {
+    /**
+     * Create time.
+     */
+    @Column(name = "create_time", columnDefinition = "timestamp default CURRENT_TIMESTAMP")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createTime;
+
+    /**
+     * Update time.
+     */
+    @Column(name = "update_time", columnDefinition = "timestamp default CURRENT_TIMESTAMP")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updateTime;
+
+    /**
+     * Delete flag.
+     */
+    @Column(name = "deleted", columnDefinition = "TINYINT default 0")
+    private Boolean deleted = false;
+
+    @PrePersist
+    protected void prePersist() {
+        deleted = false;
+        Date now = DateUtils.now();
+        if (createTime == null) {
+            createTime = now;
+        }
+
+        if (updateTime == null) {
+            updateTime = now;
+        }
+    }
+
+    @PreUpdate
+    protected void preUpdate() {
+        updateTime = new Date();
+    }
+
+    @PreRemove
+    protected void preRemove() {
+        updateTime = new Date();
+    }
+}
