05cd9f5b288f0a47a4a0adfaec17557f9edd8e56
peiyunluo <peiyunluo@icloud.com>
2020-03-25 00:11:19 +0800

    fix

--- src/main/resources/dist/static/js/chunk-74c3f564.1545f99c.js
+++
@@ -1,1 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-74c3f564"],{"8d8b0":function(t,e,n){"use strict";n.r(e);var c=function(t,e){var n=e._c;return n("div",{staticStyle:{padding:"30px"}},[n("el-alert",{attrs:{closable:!1,title:"menu 1-3",type:"success"}})],1)},l=[],s=n("623f"),u={},a=Object(s["a"])(u,c,l,!0,null,null,null);e["default"]=a.exports}}]);
